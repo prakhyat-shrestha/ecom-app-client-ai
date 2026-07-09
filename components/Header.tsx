73f4e9d2198887c2bd1b44fb576f4e4543cb66a7
prakhyat-shrestha <erprakhyat@gmail.com>
2026-07-09 11:53:06 +0545

    clerk signup signin modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,8 +23,8 @@ const Header = async () => {
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
           <CartIcon />
            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
+              <SignInButton mode="modal" />
+              <SignUpButton mode="modal">
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>

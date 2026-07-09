ebfa6b10123cc1d69b90804d7e7652fa90dfdad3
prakhyat-shrestha <erprakhyat@gmail.com>
2026-07-09 11:22:26 +0545

    env file added - header,footer,banner added

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,18 +12,14 @@ import CartIcon from "./new/CartIcon";
 
 const Header = async () => {
 
-
-
   return (
     <header className="bg-white sticky top-0 z-50 border-b border-b-gray-200 py-5">
       <Container className="flex items-center justify-between gap-7 text-lightColor">
         <HeaderMenu />
-        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">
-          
+        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">   
           <Logo>Tulos</Logo>
         </div>
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
-         
           <CartIcon />
         </div>
       </Container>

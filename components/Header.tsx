a1692ce4dc5e23b32a83045d91cc780b30355f94
prakhyat-shrestha <erprakhyat@gmail.com>
2026-07-11 13:30:08 +0545

    search icon and search modal added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import {  Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 import Container from "./Container";
 //import { getAllCategories, getMyOrders } from "@/sanity/helpers";
@@ -9,30 +9,30 @@ import Logo from "./new/Logo";
 import { ListOrdered } from "lucide-react";
 import CartIcon from "./new/CartIcon";
 //import MobileMenu from "./new/MobileMenu";
-
+import SearchBar from "./new/SearchBar";
 
 const Header = async () => {
-
   return (
     <header className="bg-white sticky top-0 z-50 border-b border-b-gray-200 py-5">
       <Container className="flex items-center justify-between gap-7 text-lightColor">
         <HeaderMenu />
-        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">   
+        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">
           <Logo>Tulos</Logo>
         </div>
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
           <CartIcon />
-           <Show when="signed-out">
-              <SignInButton mode="modal" />
-              <SignUpButton mode="modal">
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
+          <Show when="signed-out">
+            <SignInButton mode="modal" />
+            <SignUpButton mode="modal">
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
         </div>
       </Container>
     </header>
